b66e60c88c976ae27dab4cb9435a5894669572c9
kevinharscoet <71257291+kevinharscoet@users.noreply.github.com>
2020-10-25 18:34:46 +0100

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $("#header").hide();
+    $("#menu").hide();
+    $("#main").hide();
+    $("#footer").hide();
+
+    new TypeIt("#intro", {
+        strings: ["<em>Bonjour, je m'appelle Kévin.</em>","<em>Je suis à la recherche d'une alternance en programmation.</em>", "<em>Voici mon CV:</em>"], 
+        speed: 50,
+        waitUntilVisible: true
+      }).go();
+      setTimeout(function() {
+        $('#intro').hide();
+        $("#header").show();
+        $("#menu").show();
+        $("#main").show();
+        $("#footer").show();
+   }, 9000);
+})
